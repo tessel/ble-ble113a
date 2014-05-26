63b3026bdc443f49f62f0729046da0c4116c8534
johnnyman727 <johnnyman727@gmail.com>
2014-05-26 16:58:02 -0700

    Removes uneccessary setImmediates

--- examples/ble.js
+++ examples/ble.js
@@ -3,28 +3,10 @@ var blePort = tessel.port['A'];
 
 var ble = require('../').use(blePort);
 
-var interval;
-
 ble.on('ready', function(err) {
-	if (err) return console.log(err);
-  console.log('started advertising...');
-  ble.startAdvertising();
+  ble.startScanning();
 });
 
-ble.on('connect', function() {
-  console.log("We have a connection to master.");
-  var value = 0;
-  interval = setInterval(function iteration() {
-    var str = "Interval #" + value++;
-    console.log("Writing out: ", str);
-
-    ble.writeLocalValue(0, new Buffer(str));
-  }, 1000);
-});
-
-ble.on('disconnect', function() {
-  // Stop our interval
-  clearInterval(interval);
-  // Start advertising again
-  ble.startAdvertising();
+ble.on('discover', function(p) {
+  console.log('discovered p', p.toString());
 });
